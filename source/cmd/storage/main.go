d42632bc31aa7407d588d8f13a3ebf536502b650
dr-leee <a.leonovich@hotmail.com>
2022-10-26 13:13:17 +0700

    Внесена работа с файлами, структура storage теперь хранит мапу файлов

--- source/cmd/storage/main.go
+++ source/cmd/storage/main.go
@@ -3,9 +3,23 @@ package main
 import (
 	"fmt"
 	"github.com/dr-leee/go/source/internal/storage"
+	"log"
 )
 
 func main() {
 	st := storage.NewStorage()
-	fmt.Println("Hello, world!", st)
+
+	file, err := st.Upload("newfile.txt", []byte("Hello"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	restoredFile, err := st.GetByID(file.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//fmt.Println(st.files)
+
+	fmt.Println("File is restored", restoredFile)
 }
